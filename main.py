72991b7ee30ee05e18b94ee56d5c9f85c9cd05fa
toliko <tkcoding01@gmail.com>
2022-03-15 16:48:16 +0200

    test2

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from requests.structures import CaseInsensitiveDict
 
 url = "http://127.0.0.1/hello.html"
 
-"TEST"
+"TEST2"
 headers = CaseInsensitiveDict()
 headers["Authorization"] = "Bearer mt0dgHmLJMVQhvjpNXDyA83vA_PxH23Y"
 headers["Content-Type"] = "application/json"
@@ -20,4 +20,3 @@ i = False
 while i is not True:
     resp = requests.post(url, headers=headers, data=data)
     print(resp.status_code)
-
