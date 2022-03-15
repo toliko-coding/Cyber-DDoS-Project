02ce6c6ad31b7fc0585c6d312c94713c805e03cf
Barsheshett <73143641+Barsheshett@users.noreply.github.com>
2022-03-15 16:27:31 +0200

    Add a main file

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+from requests.structures import CaseInsensitiveDict
+
+url = "http://127.0.0.1/hello.html"
+
+headers = CaseInsensitiveDict()
+headers["Authorization"] = "Bearer mt0dgHmLJMVQhvjpNXDyA83vA_PxH23Y"
+headers["Content-Type"] = "application/json"
+
+data = """
+{
+  "Id": 12345,
+  "Customer": "John Smith",
+  "Quantity": 1,
+  "Price": 10.00
+}
+"""
+i = False
+while i is not True:
+    resp = requests.post(url, headers=headers, data=data)
+    print(resp.status_code)
+
